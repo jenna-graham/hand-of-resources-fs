18364c77e8daf5beb85c0d33cfed3860d247d8c3
jenna-graham <jclayto2@gmail.com>
2022-08-05 11:18:15 -0700

    GET route for all crystals TEST passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,14 +1,52 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET /crystals should return a list of crystals', async () => {
+    const res = await request(app).get('/crystals');
+    expect(res.status).toEqual(200);
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        name: 'Snowflake Obsidian',
+        color: 'black/white',
+        zodiac: 'Virgo',
+        properties: 'purity and balance',
+      },
+      {
+        id: '2',
+        name: 'Amethyst',
+        color: 'purple',
+        zodiac: 'Capricorn',
+        properties: 'spirituality and meditation',
+      },
+      {
+        id: '3',
+        name: 'Carnelean',
+        color: 'red',
+        zodiac: 'Taurus',
+        properties: 'stabilizing energies',
+      },
+      {
+        id: '4',
+        name: 'Malachite',
+        color: 'green',
+        zodiac: 'Scorpio',
+        properties: 'transformations',
+      },
+      {
+        id: '5',
+        name: 'Moonstone',
+        color: 'beige',
+        zodiac: 'Libra',
+        properties: 'new beginnings and confidence',
+      },
+    ]);
   });
   afterAll(() => {
     pool.end();
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,18 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP table if exists crystals;
+
+CREATE table crystals (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR NOT NULL,
+    color VARCHAR NOT NULL,
+    zodiac VARCHAR NOT NULL,
+    properties VARCHAR NOT NULL
+);
+
+INSERT INTO crystals (name, color, zodiac, properties) VALUES
+('Snowflake Obsidian', 'black/white', 'Virgo', 'purity and balance'),
+('Amethyst', 'purple', 'Capricorn', 'spirituality and meditation'),
+('Carnelean', 'red', 'Taurus', 'stabilizing energies'),
+('Malachite', 'green', 'Scorpio', 'transformations'),
+('Moonstone', 'beige', 'Libra', 'new beginnings and confidence');
