bc24a30be2635f3ae58850036dee2cd0f4fbe32c
jenna-graham <jclayto2@gmail.com>
2022-08-05 15:04:06 -0700

    GET route for all songs TEST passing

---
+++ __tests__/songs.test.js
@@ -0,0 +1,57 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('Get /songs should return a list of songs', async () => {
+    const res = await request(app).get('/songs');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      {
+        id: '1',
+        title: 'Is It a Crime?',
+        artist: 'Sade',
+        released: 1985,
+      },
+      {
+        id: '2',
+        title: 'Say Yes',
+        artist: 'Floetry',
+        released: 2002,
+      },
+      {
+        id: '3',
+        title: 'Call My Name',
+        artist: 'Prince',
+        released: 2004,
+      },
+      {
+        id: '4',
+        title: 'Eternal Light',
+        artist: 'Free Nationals',
+        released: 2019,
+      },
+      {
+        id: '5',
+        title: 'Orange Moon',
+        artist: 'Erykah Badu',
+        released: 2000,
+      },
+      {
+        id: '6',
+        title: 'Gaze',
+        artist: 'Sweetback',
+        released: 1996,
+      },
+    ]);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP table if exists crystals;
+DROP table if exists songs;
 
 CREATE table crystals (
     id BIGINT GENERATED ALWAYS AS IDENTITY,
@@ -9,6 +10,12 @@ CREATE table crystals (
     zodiac VARCHAR NOT NULL,
     properties VARCHAR NOT NULL
 );
+CREATE TABLE songs (
+    id BIGINT GENERATED ALWAYS AS IDENTITY,
+    title VARCHAR NOT NULL,
+    artist VARCHAR NOT NULL,
+    released INT  
+);
 
 INSERT INTO crystals (name, color, zodiac, properties) VALUES
 ('Snowflake Obsidian', 'black/white', 'Virgo', 'purity and balance'),
@@ -16,3 +23,11 @@ INSERT INTO crystals (name, color, zodiac, properties) VALUES
 ('Carnelean', 'red', 'Taurus', 'stabilizing energies'),
 ('Malachite', 'green', 'Scorpio', 'transformations'),
 ('Moonstone', 'beige', 'Libra', 'new beginnings and confidence');
+
+INSERT INTO songs (title, artist, released) VALUES
+('Is It a Crime?', 'Sade', 1985),
+('Say Yes', 'Floetry', 2002),
+('Call My Name', 'Prince', 2004),
+('Eternal Light', 'Free Nationals', 2019),
+('Orange Moon', 'Erykah Badu', 2000),
+('Gaze', 'Sweetback', 1996);
