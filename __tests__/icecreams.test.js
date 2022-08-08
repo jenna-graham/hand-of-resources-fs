76f94b27532194469cd117876b56627eb25e6dc5
jenna-graham <jclayto2@gmail.com>
2022-08-07 21:44:59 -0700

    CI test

--- __tests__/icecreams.test.js
+++ __tests__/icecreams.test.js
@@ -2,7 +2,6 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const ice_creams = require('../lib/controllers/ice_creams');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
